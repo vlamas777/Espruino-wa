b6d478accea2ce0bb68f17474f74282b7a88a167
Glukocil <glukocil@mail.ru>
2026-09-03 22:05:45 +0300

    ifdef ESP32 added

--- libs/network/jswrap_wifi.h
+++ libs/network/jswrap_wifi.h
@@ -31,8 +31,10 @@ JsVar *jswrap_wifi_getHostname(JsVar *jsCallback);
 void   jswrap_wifi_setHostname(JsVar *jsHostname, JsVar *jsCallback);
 void   jswrap_wifi_getHostByName(JsVar *jsHostname, JsVar *jsCallback);
 void   jswrap_wifi_ping(JsVar *jsHostname, JsVar *jsCallback);
-void   jswrap_wifi_setSNTPcb(JsVar *jsServer, JsVar *jsZone, JsVar *jsCallback);
 void   jswrap_wifi_setSNTP(JsVar *jsServer, JsVar *jsZone);
-JsVar *jswrap_wifi_getSNTPstatus(void);
+#ifdef ESP32
+    void   jswrap_wifi_setSNTPcb(JsVar *jsServer, JsVar *jsZone, JsVar *jsCallback);
+    JsVar *jswrap_wifi_getSNTPstatus(void);
+#endif
 void   jswrap_wifi_setIP(JsVar *jsSettings, JsVar *jsCallback);
 void   jswrap_wifi_setAPIP(JsVar *jsSettings, JsVar *jsCallback);
